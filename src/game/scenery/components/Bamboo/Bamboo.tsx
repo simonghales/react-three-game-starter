1bf9ab7bcfb15ea8f98d659f393c83ff99f042f8
Simon Hales <simonghales@gmail.com>
2021-01-13 16:09:18 +1000

    bamboo

--- src/game/scenery/components/Bamboo/Bamboo.tsx
+++ src/game/scenery/components/Bamboo/Bamboo.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef} from "react"
+import React from "react"
 import {degToRad} from "../../../../utils/angles";
 import {SingleInstance} from "../../../../temp/instancing/InstancingProvider";
 
